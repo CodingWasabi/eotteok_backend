dcdc23a2552e4f8c247cfd3e7a8fdafd1c989bd2
mac_daehwan2yo <daehwan7989@naver.com>
2022-03-23 20:08:18 +0900

    init : 개발 환경 세팅

---
+++ src/main/java/com/codingwasabi/howtodo/HowtodoApplication.java
@@ -0,0 +1,13 @@
+package com.codingwasabi.howtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowtodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowtodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingwasabi/howtodo/HowtodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingwasabi.howtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowtodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
