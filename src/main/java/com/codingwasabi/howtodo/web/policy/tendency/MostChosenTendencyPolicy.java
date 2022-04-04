9ef9a5d9458f9073ae6fa34c44b4855d63734736
mac_daehwan2yo <daehwan7989@naver.com>
2022-04-04 22:23:11 +0900

    refactoring : 알고리즘  구현체 초기화

--- src/main/java/com/codingwasabi/howtodo/web/policy/tendency/MostChosenTendencyPolicy.java
+++ src/main/java/com/codingwasabi/howtodo/web/policy/tendency/MostChosenTendencyPolicy.java
@@ -11,19 +11,7 @@ public class MostChosenTendencyPolicy implements TendencyPolicy {
 	@Override
 	// 완성되지않음, 배포 테스트 용으로 우선 커밋, 추후에 수정되어 재 배포 -> 기능이 실행되지 않을것이다.
 	public int setUp(List<Integer> answers, int studyDegree) {
-		int[] questions = new int[QUESTION_COUNT + 1];
-		int tendency = 0;
-		int mostCount = 0;
 
-		answers.forEach(chosen -> questions[chosen]++);
-
-		for (int question = 1; question <= QUESTION_COUNT; question++) {
-			if (mostCount < questions[question]) {
-				mostCount = questions[question];
-				tendency = question;
-			}
-		}
-
-		return tendency;
+		return 0;
 	}
 }
