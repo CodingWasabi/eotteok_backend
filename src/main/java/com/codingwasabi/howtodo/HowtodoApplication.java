b3bd54f0e0cb0c89901c2935bd23a2a93e7e0f0f
mac_daehwan2yo <daehwan7989@naver.com>
2022-04-11 18:09:42 +0900

    chore (#3) : datasource : MySQL 연결

--- src/main/java/com/codingwasabi/howtodo/HowtodoApplication.java
+++ src/main/java/com/codingwasabi/howtodo/HowtodoApplication.java
@@ -1,10 +1,19 @@
 package com.codingwasabi.howtodo;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HowtodoApplication {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HowtodoApplication.class, args);
