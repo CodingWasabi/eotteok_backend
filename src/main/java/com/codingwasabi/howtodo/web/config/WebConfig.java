1151f3f4da275c5508b8c66ad7b0bc93858ef512
mac_daehwan2yo <daehwan7989@naver.com>
2022-04-11 10:01:07 +0900

    feat : CORS 추가

--- src/main/java/com/codingwasabi/howtodo/web/config/WebConfig.java
+++ src/main/java/com/codingwasabi/howtodo/web/config/WebConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.codingwasabi.howtodo.security.resolver.LoginAccountArgumentResolver;
@@ -18,5 +19,12 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(loginUserArgumentResolver);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("*")
+			.allowedMethods("*");
 	}
 }
