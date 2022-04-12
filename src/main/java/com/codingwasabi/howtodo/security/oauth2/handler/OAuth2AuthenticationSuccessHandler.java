7a066fb0c634aaab3f14e14e81f6699e56d0a612
mac_daehwan2yo <daehwan7989@naver.com>
2022-04-12 17:34:01 +0900

    chore

--- src/main/java/com/codingwasabi/howtodo/security/oauth2/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/codingwasabi/howtodo/security/oauth2/handler/OAuth2AuthenticationSuccessHandler.java
@@ -1,8 +1,10 @@
 package com.codingwasabi.howtodo.security.oauth2.handler;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -16,7 +18,16 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
 	public void onAuthenticationSuccess(HttpServletRequest request,
 										HttpServletResponse response,
 										Authentication authentication) throws IOException, ServletException {
-		response.encodeRedirectURL("http://localhost:3000/result");
+		// response.encodeRedirectURL("http://localhost:3000/result");
+		String requestCookie = Arrays.stream(request.getCookies())
+								  .filter(cookie -> cookie.getName()
+														  .equals("JSESSIONID"))
+								  .findAny()
+								  .get()
+								  .getValue();
+
+		response.addCookie(new Cookie("localhost:3000", requestCookie));
+		response.encodeURL("http://localhost:3000/result");
 		// response.setStatus(HttpServletResponse.SC_OK);
 	}
 }
