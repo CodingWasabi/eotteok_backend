f070b34815ab1d1f6553e6d5e5560ce1a3cef51e
KRBeomJunKim <wka8963@naver.com>
2022-05-24 22:31:05 +0900

    test: Exam Entity Test 추가

--- src/test/java/com/codingwasabi/howtodo/unit/domain/exam/ExamTest.java
+++ src/test/java/com/codingwasabi/howtodo/unit/domain/exam/ExamTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -13,7 +14,7 @@ import com.codingwasabi.howtodo.unit.utils.EntityFactory;
 import com.codingwasabi.howtodo.web.account.entity.Account;
 import com.codingwasabi.howtodo.web.exam.entity.Exam;
 
-@DisplayName("Exam, 도메인 단위 테스트")
+@DisplayName("Exam : Entity 단위 테스트")
 public class ExamTest {
 
 	private Account account;
@@ -23,37 +24,234 @@ public class ExamTest {
 		account = EntityFactory.Account_생성();
 	}
 
-	@DisplayName("Exam 인스턴스 생성")
 	@Test
-	void Exam_생성() {
-		// when
+	void 정상_builder_인스턴스생성() {
+		// Given
+		String name = "과목";
+		LocalDateTime dueDateTime = LocalDateTime.now();
+		int studyDegree = 3;
+
+		// When
 		Exam exam = Exam.builder()
-						.name("과목")
+						.name(name)
 						.account(account)
-						.dueDateTime(LocalDateTime.now())
-						.studyDegree(3)
+						.dueDateTime(dueDateTime)
+						.studyDegree(studyDegree)
 						.build();
 
 		// then
-		assertThat(exam.getName()).isEqualTo("과목");
-		assertThat(exam.getStudyDegree()).isEqualTo(3);
+		assertThat(exam.getName()).isEqualTo(name);
+		assertThat(exam.getAccount()).isEqualTo(account);
+		assertThat(exam.getDueDateTime()).isEqualTo(dueDateTime);
+		assertThat(exam.getStudyDegree()).isEqualTo(studyDegree);
 	}
 
-	@DisplayName("과목 d-day 구하기")
 	@Test
-	void D_Day_계산() {
+	void 정상_getDDay_같은date_0() {
 		// given
+		LocalDateTime dueDateTime = LocalDateTime.of(2022, 4, 20, 14, 30);
+
 		Exam exam = Exam.builder()
 						.name("과목")
 						.account(account)
-						.dueDateTime(LocalDateTime.of(2022, 04, 20, 14, 30))
+						.dueDateTime(dueDateTime)
 						.studyDegree(3)
 						.build();
 
 		// when
-		int dDay = exam.getDDay(LocalDate.of(2022, 04, 15));
+		int result = exam.getDDay(dueDateTime.toLocalDate());
 
 		// then
-		assertThat(dDay).isEqualTo(5);
+		assertThat(result).isEqualTo(0);
+	}
+
+	@Test
+	void 정상_getDDay_하루빠른date_1() {
+		// given
+		LocalDateTime dueDateTime = LocalDateTime.of(2022, 4, 20, 14, 30);
+
+		Exam exam = Exam.builder()
+						.name("과목")
+						.account(account)
+						.dueDateTime(dueDateTime)
+						.studyDegree(3)
+						.build();
+
+		// when
+		int result = exam.getDDay(dueDateTime.plus(-1, ChronoUnit.DAYS).toLocalDate());
+
+		// then
+		assertThat(result).isEqualTo(1);
+	}
+
+	@Test
+	void 정상_getDDay_하루늦은date_minus1() {
+		// given
+		LocalDateTime dueDateTime = LocalDateTime.of(2022, 4, 20, 14, 30);
+
+		Exam exam = Exam.builder()
+						.name("과목")
+						.account(account)
+						.dueDateTime(dueDateTime)
+						.studyDegree(3)
+						.build();
+
+		// when
+		int result = exam.getDDay(dueDateTime.plus(1, ChronoUnit.DAYS).toLocalDate());
+
+		// then
+		assertThat(result).isEqualTo(-1);
+	}
+
+	@Test
+	void 정상_isMid_dueDateTime의day가10인경우_true() {
+		// given
+		LocalDateTime dueDateTime = LocalDateTime.of(2022, 4, 10, 14, 30);
+
+		Exam exam = Exam.builder()
+						.name("과목")
+						.account(account)
+						.dueDateTime(dueDateTime)
+						.studyDegree(3)
+						.build();
+
+		// when
+		boolean result = exam.isMid();
+
+		// then
+		assertThat(result).isTrue();
+	}
+
+	@Test
+	void 정상_isMid_dueDateTime의day가20인경우_true() {
+		// given
+		LocalDateTime dueDateTime = LocalDateTime.of(2022, 4, 20, 14, 30);
+
+		Exam exam = Exam.builder()
+						.name("과목")
+						.account(account)
+						.dueDateTime(dueDateTime)
+						.studyDegree(3)
+						.build();
+
+		// when
+		boolean result = exam.isMid();
+
+		// then
+		assertThat(result).isTrue();
+	}
+
+	@Test
+	void 정상_isMid_dueDateTime의day가9인경우_false() {
+		// given
+		LocalDateTime dueDateTime = LocalDateTime.of(2022, 4, 9, 14, 30);
+
+		Exam exam = Exam.builder()
+						.name("과목")
+						.account(account)
+						.dueDateTime(dueDateTime)
+						.studyDegree(3)
+						.build();
+
+		// when
+		boolean result = exam.isMid();
+
+		// then
+		assertThat(result).isFalse();
+	}
+
+	@Test
+	void 정상_isMid_dueDateTime의day가21인경우_false() {
+		// given
+		LocalDateTime dueDateTime = LocalDateTime.of(2022, 4, 21, 14, 30);
+
+		Exam exam = Exam.builder()
+						.name("과목")
+						.account(account)
+						.dueDateTime(dueDateTime)
+						.studyDegree(3)
+						.build();
+
+		// when
+		boolean result = exam.isMid();
+
+		// then
+		assertThat(result).isFalse();
+	}
+
+	@Test
+	void 정상_isFront_dueDateTime의day가10인경우_false() {
+		// given
+		LocalDateTime dueDateTime = LocalDateTime.of(2022, 4, 10, 14, 30);
+
+		Exam exam = Exam.builder()
+						.name("과목")
+						.account(account)
+						.dueDateTime(dueDateTime)
+						.studyDegree(3)
+						.build();
+
+		// when
+		boolean result = exam.isFront();
+
+		// then
+		assertThat(result).isFalse();
+	}
+
+	@Test
+	void 정상_isFront_dueDateTime의day가9인경우_true() {
+		// given
+		LocalDateTime dueDateTime = LocalDateTime.of(2022, 4, 9, 14, 30);
+
+		Exam exam = Exam.builder()
+						.name("과목")
+						.account(account)
+						.dueDateTime(dueDateTime)
+						.studyDegree(3)
+						.build();
+
+		// when
+		boolean result = exam.isFront();
+
+		// then
+		assertThat(result).isTrue();
+	}
+
+	@Test
+	void 정상_isBack_dueDateTime의day가20인경우_false() {
+		// given
+		LocalDateTime dueDateTime = LocalDateTime.of(2022, 4, 20, 14, 30);
+
+		Exam exam = Exam.builder()
+						.name("과목")
+						.account(account)
+						.dueDateTime(dueDateTime)
+						.studyDegree(3)
+						.build();
+
+		// when
+		boolean result = exam.isBack();
+
+		// then
+		assertThat(result).isFalse();
+	}
+
+	@Test
+	void 정상_isBack_dueDateTime의day가21인경우_true() {
+		// given
+		LocalDateTime dueDateTime = LocalDateTime.of(2022, 4, 21, 14, 30);
+
+		Exam exam = Exam.builder()
+						.name("과목")
+						.account(account)
+						.dueDateTime(dueDateTime)
+						.studyDegree(3)
+						.build();
+
+		// when
+		boolean result = exam.isBack();
+
+		// then
+		assertThat(result).isTrue();
 	}
 }
