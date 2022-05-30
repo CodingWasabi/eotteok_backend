32f108c82a167158c905df8dd9899eee7fe0e35a
mac_daehwan2yo <daehwan7989@naver.com>
2022-05-31 00:38:27 +0900

    fix : 어노테이션 추가

--- src/main/java/com/codingwasabi/howtodo/web/calendar/CalendarRepository.java
+++ src/main/java/com/codingwasabi/howtodo/web/calendar/CalendarRepository.java
@@ -3,7 +3,9 @@ package com.codingwasabi.howtodo.web.calendar;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.codingwasabi.howtodo.web.account.entity.Account;
 import com.codingwasabi.howtodo.web.calendar.entity.Calendar;
@@ -12,6 +14,8 @@ public interface CalendarRepository extends JpaRepository<Calendar, Long> {
 	Optional<Calendar> findByAccount(Account account);
 
 
+	@Modifying
+	@Transactional
 	@Query("delete from Calendar c where c.account.id=:accountId")
 	void deleteByAccountId(Long accountId);
 
